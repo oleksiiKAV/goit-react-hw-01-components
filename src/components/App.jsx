22eca3264d300c05368d4560357202e05d74060b
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-05-22 18:11:21 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import FriendList from './FriendList/FriendList';
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
+// import transactions from '../data/transactions.json';
 
 // React homework 1 template
 export const App = () => {
